1026aa8e9598ddc486b38c29614cc9f3051582e4
Lika4y <guralika@gmail.com>
2023-04-14 13:24:12 +0300

    add edit-methods

--- js/app.js
+++ js/app.js
@@ -294,7 +294,7 @@ class Request {
     }
   }
 
-  delete(token, url, cardId, obj){
+  delete(token, url, cardId){
     try{
       let result = fetch(`${url}/${cardId}`, {
       method: 'DELETE',
@@ -322,9 +322,6 @@ class Request {
     } catch(e){
       console.log(e.message);
     }
-    
-  // .then(response => response.json())
-  // .then(response => console.log(response))
   }
 }
 
@@ -486,7 +483,7 @@ createVisitBtn.addEventListener("click", (e) => {
   const btnSaveVisit = document.getElementById(`save-${counter}`); // кнопка сохранить в модалке создания визита. при нажатии на нее - пост запрос и создание карточки
   btnSaveVisit.addEventListener("click", (e) => {
     e.preventDefault();
-    modal2.closeModal(); // закрыла модалку.
+    modal2.closeModal(); 
     console.log("здесь делаем пост запрос и выводим карточку на стену");
     let cardObj;
     if (formSelect.value === "dentist") {
@@ -546,7 +543,8 @@ createVisitBtn.addEventListener("click", (e) => {
 
     newRequest.post(url, cardObj, TOKEN)      
     .then(response => response.json())
-    .then(data => carder.renderCard(data))
+    .then(data => carder.renderCard(data));
+    
   });
 });
 
@@ -565,7 +563,7 @@ class Card {
         <div id='edit-${counter}' class="small-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M11.013 1.427a1.75 1.75 0 0 1 2.474 0l1.086 1.086a1.75 1.75 0 0 1 0 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 0 1-.927-.928l.929-3.25c.081-.286.235-.547.445-.758l8.61-8.61Zm.176 4.823L9.75 4.81l-6.286 6.287a.253.253 0 0 0-.064.108l-.558 1.953 1.953-.558a.253.253 0 0 0 .108-.064Zm1.238-3.763a.25.25 0 0 0-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 0 0 0-.354Z"></path></svg></div>
         <div id='del-${counter}' class="small-button del-card"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M5.72 5.72a.75.75 0 0 1 1.06 0L12 10.94l5.22-5.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L13.06 12l5.22 5.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L12 13.06l-5.22 5.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L10.94 12 5.72 6.78a.75.75 0 0 1 0-1.06Z"></path></svg></div>
         </div>
-        <h5>${lastName} ${name} ${surname}</h5>
+        <h5 id="name-${counter}">${lastName} ${name} ${surname}</h5>
         </div>
         <div class="card-body">
          <h5 class="card-title">Доктор: ${getDoctor(doctor)}</h5>
@@ -574,15 +572,16 @@ class Card {
   
     cardContainer.prepend(card);
 
-    let showMoreBtn = document.getElementById(`show-more-${counter}`); 
+    let showMoreBtn = document.getElementById(`show-more-${counter}`);
     showMoreBtn.addEventListener("click", ()=> {// клик на кнопку показать больше
              carder.clickOnShowMore(showMoreBtn, card, doctor, age, diseases, index, press, data, goal, description, ugency);
     })
     let delBtn = document.getElementById(`del-${counter}`);
     let editBtn = document.getElementById(`edit-${counter}`);
     let doctorName = document.querySelector(".card-title");
+    let nameClient = document.getElementById(`name-${counter}`);
     carder.deleteCard(delBtn, card, id); //  метод удаления карточки 
-    carder.editCard(editBtn, doctorName, id);  // метод изменения карточки 
+    carder.editCard(editBtn, doctorName, id, card, nameClient, showMoreBtn);  // метод изменения карточки 
     return card;
   }
 
@@ -598,34 +597,172 @@ class Card {
       }
      })
      });
-
   }
 
   
-  editCard(btn, doctor, cardId){
-     
+  editCard(btn, doctorStatus, cardId, card, nameClient, showMoreBtn){
      btn.addEventListener("click", () => {
-      
       counter1++;
-      const modal3 = new Modal(`window-${counter1}`, `edit-btn-${counter1}`);
-      document.body.append(modal3.render("Редактировать карточку", visit.render(cardEditForm(doctor)), "Отменить", "Сохранить"));
+      const modal3 = new Modal(`window-edit`, `edit-btn-${counter1}`);
+      document.body.append(modal3.render("Редактировать карточку", visit.render(cardEditForm(doctorStatus)), "Отменить", "Сохранить"));
       modal3.openModal();
-let modalBody = document.getElementById(`window-${counter1}`);
 
+     let doctorSelect = document.querySelectorAll(".visit-doctor");
+     let arr = Array.from(doctorSelect);
+     let lastSelect = arr.at(-1);
 
-modalBody.addEventListener("change", (e)=> {
-        console.log(e.target.value, e.currentTarget);
+     const dentist2 = new VisitDentist(
+      "Цель визита",
+      "Выберите врача",
+      "Краткое описание",
+      "Выберите срочность",
+      "ФИО клиента",
+      "Дата визита"
+    );
   
-})
+    modal3.changeModal(dentist2.renderDentist());
 
+// отслеживаю изменения в окне "Редактировать карточку" при смене врача.
+       lastSelect.addEventListener("change", (e)=> {
+         let secondBody = document.querySelectorAll(".second-modal-body");
 
-           
+        if(e.target.value === "dentist"){
+          let dentist3 = new VisitDentist();
+          secondBody.forEach(el => el.remove());
+          modal3.changeModal(dentist3.renderDentist());
+          lastSelect = e.target.value;
+        } else if(e.target.value === "cardiologist"){
+          let cardiologist2 = new VisitCardiologist();
+          secondBody.forEach(el => el.remove());
+          modal3.changeModal(cardiologist2.renderCardiologist());
+          lastSelect = e.target.value;
+        } else {
+          let therapist2 = new VisitTherapist();
+          secondBody.forEach(el => el.remove());
+          modal3.changeModal(therapist2.renderTherapist());
+          lastSelect = e.target.value;
+        }
+        return lastSelect;
+ })
+
+function getValueInputs(){
+let obj = {};
+let goal = getLastValue(".visit-goal");
+let ugency = getLastValue(".visit-ugency");
+let data = getLastValue(".visit-data");
+let description = getLastValue(".visit-description");
+let lastName = getLastValue(".visit-lastName");
+let name = getLastValue(".visit-name");
+let surname = getLastValue(".visit-surname");
+
+obj = {
+  goal: goal.value,
+  ugency: ugency.value,
+  data: data.value,
+  description: description.value,
+  lastName: lastName.value,
+  name: name.value,
+  surname: surname.value,
+  doctor: lastSelect,
+}
+return obj;
+
+ }
+
       //далее при нажатии на кнопку сохранить, отправляем пост запрос и меняем данные в этой карте. 
-      let saveChanges = document.getElementById(`edit-btn-${counter1}`);
+let saveChanges = document.getElementById(`edit-btn-${counter1}`);
 
-      saveChanges.addEventListener("click", ()=> {
-        console.log("отсюда полетит put-запрос на изменение карты на сервере");
+saveChanges.addEventListener("click", (e)=> {
+        e.preventDefault();
+        console.log(getValueInputs());
         modal3.closeModal();
+        let fullobj;
+        if(lastSelect === "dentist"){
+          let {goal, doctor, ugency, data, description, lastName, name, surname } = getValueInputs();
+          let lastVisit = getLastValue(".last-visit");
+
+          fullobj = {
+            goal: goal,
+            ugency: ugency,
+            data: data,
+            description: description,
+            lastName: lastName,
+            name: name,
+            surname: surname,
+            doctor: doctor,
+            dataLastVisit: lastVisit.value,
+          };
+        } else if (lastSelect === "cardiologist"){
+          let {goal, doctor, ugency, data, description, lastName, name, surname } = getValueInputs();
+          let press = getLastValue(".press");
+          let age = getLastValue(".age");
+          let index = getLastValue(".index");
+          let diseases = getLastValue(".diseases");
+
+          fullobj = {
+            goal: goal,
+            ugency: ugency,
+            data: data,
+            description: description,
+            lastName: lastName,
+            name: name,
+            surname: surname,
+            doctor: doctor,
+            press: press.value,
+            index: index.value,
+            diseases: diseases.value,
+            age: age.value,
+          };
+        } else {
+          let {goal, doctor, ugency, data, description, lastName, name, surname } = getValueInputs();
+          let age = getLastValue(".age");
+
+          fullobj = {
+            goal: goal,
+            ugency: ugency,
+            data: data,
+            description: description,
+            lastName: lastName,
+            name: name,
+            surname: surname,
+            doctor: doctor,
+            age: age.value,
+          }
+        }
+        console.log(fullobj);
+        const putRequest = new Request();
+        putRequest.put(TOKEN, url, cardId, fullobj)
+        .then(response => response.json())
+        .then(obj => {
+          let { lastName, name, surname, doctor, age, diseases, index, data, press, goal, description, ugency } = obj;
+            nameClient.textContent = `${lastName} ${name} ${surname}`;
+            doctorStatus.textContent = `${getDoctor(doctor)}`;
+            let listGroup = document.querySelector(".list-group");
+            if(listGroup === null){
+              showMoreBtn.addEventListener("click", ()=> {// клик на кнопку показать больше
+                let listGroups = document.querySelectorAll(".list-group");
+                listGroups.forEach(el => {
+                  el.remove()});
+              
+                let carder3 = new Card();
+                carder3.clickOnShowMore(showMoreBtn, card, doctor, age, diseases, index, press, data, goal, description, ugency);
+              });
+            } else {
+              let listGroups = document.querySelectorAll(".list-group");
+                listGroups.forEach(el => {
+                  el.remove()});
+              //listGroup.remove();
+              let carder2 = new Card();
+              carder2.clickOnShowMore(btn, card, doctor, age, diseases, index, press, data, goal, description, ugency);
+              btn.style.display = "flex";
+            }       
+            
+        });
+
+        let oldModal = document.getElementById("window-edit");
+        if (oldModal !== null) {
+        oldModal.remove();
+  }
       })
      })
    }
@@ -722,6 +859,18 @@ modalBody.addEventListener("change", (e)=> {
     );
     return therapist1.renderTherapist();
   }
+}
+
+//ищу значение в инпуте при редактировании карты
+function getLastValue(classes){
+  let current = document.querySelectorAll(classes);
+  let arrofCurrent = Array.from(current);
+  let lastCurrent = arrofCurrent.at(-1);
+  lastCurrent.addEventListener("change", (e)=> {
+    lastCurrent = e.target.value;
+    console.log(lastCurrent);
+  })
+  return lastCurrent;
 }
 
 const carder = new Card();
